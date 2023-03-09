f1e091ef08b75d5734a53a225e7b92353361247e
Chrism982 <chrism9822@gmail.com>
2023-03-09 19:47:08 +0100

    add listener for link

--- js.js
+++ js.js
@@ -1,15 +1,17 @@
-const burgerBtn = document.querySelector('.burger')
-const barsIco = document.querySelector('.fa-bars')
-const xIco = document.querySelector('.fa-times')
-const nav = document.querySelector('nav ul')
+const burgerBtn = document.querySelector('.burger');
+const barsIcon = document.querySelector('.fa-bars');
+const xIcon = document.querySelector('.fa-times');
+const nav = document.querySelector('nav ul');
+const link = document.querySelectorAll('li');
 
 const handleNav = () => {
-    nav.classList.toggle('active')
-    burgerBtn.classList.toggle('active')
-    barsIco.classList.toggle('hide')
-    xIco.classList.toggle('hide')
+	nav.classList.toggle('active');
+	burgerBtn.classList.toggle('active');
+	barsIcon.classList.toggle('hide');
+	xIcon.classList.toggle('hide');
+};
+
+burgerBtn.addEventListener('click', handleNav);
+link.forEach(li => li.addEventListener('click', handleNav))
 
 
-}
-
-burgerBtn.addEventListener('click', handleNav)
